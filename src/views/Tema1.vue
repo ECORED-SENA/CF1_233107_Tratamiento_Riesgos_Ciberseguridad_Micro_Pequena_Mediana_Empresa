20844010100d170a0a1d319ab2ae37dc0528b720
MarciaSolano <99497342+MarciaSolano@users.noreply.github.com>
2022-12-21 07:30:03 -0500

    Video Dirección Física

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -218,7 +218,7 @@
 
           figure
             .video
-              iframe(width="560" height="315" src="https://www.youtube.com/embed/omJPlrGbFWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+              iframe(width="560" height="315" src="https://www.youtube.com/embed/C5BpaaNij8I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     Separador
 
